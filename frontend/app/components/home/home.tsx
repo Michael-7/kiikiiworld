cc7cfd51369131aa033d8ef4882a4a2e637afa51
mmheemskerk <m.m.heemskerk@uva.nl>
2024-05-22 14:57:02 +0200

    test commit

--- frontend/app/components/home/home.tsx
+++ frontend/app/components/home/home.tsx
@@ -32,7 +32,6 @@ function getPosts(): Array<Video|Photo|Quote>  {
   // const response = await fetch("http://example.com/movies.json");
   // const movies = await response.json();
   // console.log(movies);
-
   return posts
 }
 
