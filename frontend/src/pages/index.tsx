a641cb8ed5cb73e9d9c74d7366076ab187ddcfab
mmheemskerk <michael.m.heemskerk@gmail.com>
2024-06-25 13:58:20 +0200

    dynamic font loader

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,4 +1,3 @@
-import { JetBrains_Mono, Open_Sans } from 'next/font/google'
 import { posts, categories } from '#site/content';
 import Post from "@/components/post/post";
 import { useSearchParams } from "next/navigation";
@@ -8,8 +7,7 @@ import Nav from "@/components/nav/nav";
 import Menu from "@/components/menu/menu";
 import BasePost from "@/types/post";
 
-const primaryFont = Open_Sans({ subsets: ['latin']});
-const secondaryFont = JetBrains_Mono({ subsets: ['latin'] });
+
 
 export default function Home() {
   const filter = useSearchParams()?.get('filter');
@@ -35,7 +33,7 @@ export default function Home() {
   return (
     <>
       <Nav></Nav>
-      <main id="index" className={`${primaryFont.className} ${secondaryFont.className}`}>
+      <main id="index">
         <div className="post-container">
           <div className="post-list">
             {shownPosts.map(post => <Post key={post.slug} post={post}></Post>)}
