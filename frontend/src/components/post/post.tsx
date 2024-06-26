edfae281b82bedf7fe84578c6cf085bb5b3cac41
mmheemskerk <michael.m.heemskerk@gmail.com>
2024-06-26 13:49:14 +0200

    added dark theme

--- frontend/src/components/post/post.tsx
+++ frontend/src/components/post/post.tsx
@@ -1,29 +1,30 @@
 import BasePost from "@/types/post";
 import { Categories } from "@/types/categories";
 import { MDXContent } from "../mdx/mdx";
+import Link from "next/link";
 
 function getPostSpecificHtml(post: BasePost) {
   switch (post.category) {
     case Categories.photo:
-      return <img src={post.image && getImageUrl(post.image)} className="post__content" alt={post.title} />
-      // if (post.image) {
-      //   return <Image src={post.image && getImageUrl(post.image)} alt={post.title}></Image>
-      // }
+      if (post.image) {
+        return (<Link href={post.image && getImageUrl(post.image)}>
+          <img src={post.image && getImageUrl(post.image)} className="post__content" alt={post.title} />
+        </Link>)
+      }
     case Categories.quote:
       return <MDXContent code={post.body} components={{}} />
     case Categories.video:
       return (
-        <p>{post.video}</p>
-        // <iframe className="post__video"
-        //   src={post.video}
-        //   title="YouTube video player"
-        //   frameBorder="0"
-        //   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        //   referrerPolicy="strict-origin-when-cross-origin" allowFullScreen>
-        // </iframe>
+        // <p>{post.video}</p>
+        <iframe className="post__video"
+          src={post.video}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin" allowFullScreen>
+        </iframe>
         )
     case Categories.story:
-      // return <p>{post.description}</p>
       return <MDXContent code={post.body} components={{}} />
     case Categories.project:
       return <MDXContent code={post.body} components={{}} />
