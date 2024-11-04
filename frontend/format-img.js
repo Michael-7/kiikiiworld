cb69b693cc1976c748a093daaf04271914b4b5e8
mmheemskerk <michael.m.heemskerk@gmail.com>
2024-11-04 10:45:20 +0100

    new photo

--- frontend/format-img.js
+++ frontend/format-img.js
@@ -7,10 +7,16 @@ async function readFiles() {
   const files = fs.readdirSync('./format-img')
 
   files.forEach(file => {
-    if(file.includes('.jpg') || file.includes('.png')) {
-        sharp(`./format-img/${file}`).resize(1200, undefined, {withoutEnlargement: true}).toFile(`./format-img/out/${file}`);
-        console.log(file + ' done!');
-      }
+    if (
+      file.includes(".jpg") ||
+      file.includes(".png") ||
+      file.includes(".JPG")
+    ) {
+      sharp(`./format-img/${file}`)
+        .resize(1200, undefined, { withoutEnlargement: true })
+        .toFile(`./format-img/out/${file}`);
+      console.log(file + " done!");
+    }
   });
 }
 
